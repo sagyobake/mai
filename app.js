f04921bb496fd5e544259b707c0d48cc0a7b643e
sagyobake <darkmatter8128@gmail.com>
2025-01-13 22:18:43 +0900

    upgrade

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ Deno.serve({
     port: 443,
     handler: async (request) => {
         console.log(request.url);
-        const file = await Deno.open(`${request.url}`, { read: true });
+        const file = await Deno.open('./index.html', { read: true });
         return new Response(file.readable);
 
     },
