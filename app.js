df178b9447c0e2d61e7860e38971baffab8894c8
sagyobake <sagyobake@localhost.localdomain>
2025-02-16 23:11:53 +0900

    upgrade

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-Deno.serve(async (req) => {
+Deno.serve({ port: 4321 }, async (req) => {
 
     const html = await Deno.readFile("./index.html");
 
     if (req.headers.get("upgrade") != "websocket") {
-        return new Response(html, { status: 200 });
+        return new Response(html);
     }
     const { socket, response } = Deno.upgradeWebSocket(req);
+
+
     socket.addEventListener("open", () => {
-        console.log("a client connected!");
+
+
+        //関数計算機
+        const heartGenerator = () => {
+            for (let i = -1; i <= 1.6; i += 0.015) { //0.015
+                for (let j = -1; j <= 1.6; j += 0.015) { //0.015
+                    const a = i ** 2 + (j - Math.cbrt(i ** 2)) ** 2;
+                    if (a < 0.99 && a < 1.01) {
+                        socket.send(JSON.stringify({ i, j }));
+                    }
+                }
+            }
+        }
+        heartGenerator();
+        heartGenerator();
+        //---------------------
+
     });
+
     socket.addEventListener("message", (event) => {
-        socket.send('pong');
         if (event.data === "ping") {
             socket.send("pong");
         }
     });
+
     return response;
 });
+
