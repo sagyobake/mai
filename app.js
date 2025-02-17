318fb673b8a80444bc30dffe4794b0c54fafabcd
sagyobake <sagyobake@localhost.localdomain>
2025-02-17 23:34:45 +0900

    upgrade

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-Deno.serve({ port: 4321 }, async (req) => {
+Deno.serve(async (req) => {
 
     const html = await Deno.readFile("./index.html");
 
@@ -10,20 +10,25 @@ Deno.serve({ port: 4321 }, async (req) => {
 
     socket.addEventListener("open", () => {
 
+        function getRandomInt(min, max) {
+            const minCeiled = Math.ceil(min);
+            const maxFloored = Math.floor(max);
+            return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // 上限は除き、下限は含む
+        }
 
         //関数計算機
         const heartGenerator = () => {
-            for (let i = -1; i <= 1.6; i += 0.015) { //0.015
-                for (let j = -1; j <= 1.6; j += 0.015) { //0.015
+            for (let i = -1; i <= 1.6; i += 0.002) { //0.015
+                for (let j = -1; j <= 1.6; j += 0.002) { //0.015
                     const a = i ** 2 + (j - Math.cbrt(i ** 2)) ** 2;
-                    if (a < 0.99 && a < 1.01) {
-                        socket.send(JSON.stringify({ i, j }));
+                    if (a > 0.995 && a < 1.005) {
+                        socket.send(JSON.stringify({ i, j}));
                     }
                 }
             }
         }
         heartGenerator();
-        heartGenerator();
+
         //---------------------
 
     });
